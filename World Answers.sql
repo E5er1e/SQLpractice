698134be62e12471d9e19b47a6a3214c7309a12f
Kyle Preston <kspreston98@gmail.com>
2023-10-20 12:16:26 +0100

    Answered all the world questions

--- World Answers.sql
+++ World Answers.sql
@@ -60,3 +60,84 @@ select name, SurfaceArea
 from country
 order by SurfaceArea
 limit 10;
+
+-- 11
+select name 
+from city
+where countrycode = "JPN"
+order by population desc
+limit 5;
+
+-- 12
+SET SQL_SAFE_UPDATES=0;
+update country
+set headofstate = "Elizabeth II" 
+where headofstate = "Elisabeth II";
+
+select name, code
+from country
+where headofstate ="Elizabeth II";
+
+-- 13
+select name, population/surfacearea as ratio
+from country
+where population > 0 
+order by ratio 
+limit 10;
+
+-- 14
+select distinct language
+from countrylanguage;
+
+-- 15
+select name, GNP
+from country
+order by GNP desc
+limit 10;
+
+-- 16
+select country.name, count(countrylanguage.countrycode) as count
+from countrylanguage
+inner join country
+on country.code = countrylanguage.countrycode
+group by countrylanguage.countrycode
+order by count desc
+limit 10;
+
+-- 17
+select country.name
+from countrylanguage
+inner join country
+on country.code = countrylanguage.countrycode
+where language = "German" and percentage > 50;
+
+-- 18
+select name 
+from country
+where LifeExpectancy is not null and LifeExpectancy > 0
+order by LifeExpectancy
+limit 1;
+
+-- 19
+select governmentform, count(governmentform)
+from country
+group by GovernmentForm
+order by count(governmentform) desc
+limit 3;
+
+-- 20
+select count(name)
+from country
+where indepyear is not null;
+
+-- 20
+
+
+
+
+
+
+
+
+
+ 
